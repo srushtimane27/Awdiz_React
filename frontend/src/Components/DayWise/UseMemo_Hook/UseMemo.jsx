7072e75b3670cc05be9112137b5afe7ed46dea69
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2023-12-22 15:20:29 +0530

    Use_Memo_Hook

---
+++ frontend/src/Components/DayWise/UseMemo_Hook/UseMemo.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const UseMemo = () => {
+
+    const[count, setCount] = useState(0);
+    const[todos, setTodos] = useState(["New Todo"]);
+
+
+
+    const calculation = expensiveCalculation(count);
+
+    const increment = () => {
+        setCount((c) => c + 1);
+    };
+
+    const addTodo = () => {
+        setTodos((t) => [...t, "New Todo"]);
+    };
+    
+  return (
+    <div>
+        <div>
+            <h2>My Todos :</h2>
+            {todos.map((todo, index) => {
+                return <p key={index}>{todo}</p>;
+            })}
+            <button style={{height:"50px", width:"100px", border: '2px solid black', fontSize: "20px"}} onClick={addTodo}>addTodo</button>
+        </div>
+        <br />
+        <hr />
+        <div>
+            Count: {count}
+            <button onClick={increment}> + </button>
+            <h2>Expensive Calculation</h2>
+            {calculation}
+        </div>
+    </div>
+  );
+};
+
+const expensiveCalculation = (num) => {
+    console.log("Lenghty Calculations...");
+    for(let i = 0; i < 10; i++){
+        num += 1;
+    }
+    return num;
+};
+
+export default UseMemo
