b9b134ae8740499ff82dfc5607ce6bfe5b5d73d9
srushtimane27 <srushtimane31@gmail.com>
2024-03-13 11:39:06 +0530

    Updates

--- batch28/src/AuthContext/AuthContextComponent.jsx
+++ batch28/src/AuthContext/AuthContextComponent.jsx
@@ -21,6 +21,22 @@ const AuthContextComponent = ({ children }) => {
 
     const [state, dispatch] = useReducer(Reducer, InitialState)
 
+    function LOGIN(data){
+        dispatch({type: "LOGIN", payload: data})
+    }
+    function LOGOUT(){
+        dispatch({type: "LOGOUT"})
+    }
+
+    async function getUserData(token){
+        try {
+            const response = {data: {success: true, userData: {name:'Srushti'}}}
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     
 
 
